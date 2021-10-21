f982f4fe4f9afbed00aa0b21fe3215df4120fa67
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-21 23:08:05 +0200

    page crud

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+  <h1>pagina di creazione</h1>
+@endsection
---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+  <h1>pagina di edit</h1>
+@endsection
