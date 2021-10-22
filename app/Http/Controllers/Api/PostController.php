4812d04b83663b31d0aa48da94987f0515d686a9
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-22 18:23:27 +0200

    show api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -40,9 +40,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        //ritoriamo il post in json
+        return response()->json($post);
     }
 
     /**
